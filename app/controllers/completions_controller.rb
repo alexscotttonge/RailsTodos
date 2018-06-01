79c74e31448bfc0a4a521b81d77e4fcd7a69f850
Alex <alex@scotttonge.com>
2018-06-01 12:02:56 +0100

    User can mark todo incomplete

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -6,6 +6,12 @@ class CompletionsController < ApplicationController
     redirect_to todos_path
   end
 
+  def destroy
+    todo.incomplete
+
+    redirect_to todos_path
+  end
+
   private
 
     def todo
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,14 @@
 class Todo < ApplicationRecord
+
   def completed?
     completed_at?
   end
 
   def completed!
     touch :completed_at
+  end
+
+  def incomplete
+    update completed_at: nil
   end
 end
